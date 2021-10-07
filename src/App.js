d8d78435c84c146ed288dc2f85143b36c1ac76e2
React_Teaching_Project <priyassss789@gmail.com>
2021-10-07 18:20:22 +0530

    Using the React Context API

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./Store/auth-context";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInformation = localStorage.getItem("isLoggedIn");
 
-
-useEffect( ()=>{
-  const storedUserLoggedInformation = localStorage.getItem("isLoggedIn");
-
-  if (storedUserLoggedInformation === '1') {
-    setIsLoggedIn(true);
-  }
-},[])
+    if (storedUserLoggedInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
@@ -26,18 +24,22 @@ useEffect( ()=>{
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
