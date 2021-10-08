fd2acc529d70e4786c2d8266cdcae6aee06c09d6
React_Teaching_Project <priyassss789@gmail.com>
2021-10-08 12:11:38 +0530

    Refactoring an input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useReducer , useContext} from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../Store/auth-context";
+import Input from "../UI/Input/Input";
 
 //////////////////////////////////////////////////
 //email
@@ -20,12 +21,11 @@ const emailReducer = (state, action) => {
 ///////////////////////////////////////////////////////
 //password
 const passwordReducer = (state, action) => {
-  if(action.type === "USER_INPUT"){
-    return { value: action.val, isValid: action.val.trim().length > 6  };
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
   }
-  if(action.type === "INPUT_BLUR"){
-    
-  return { value: state.value, isValid: state.value.trim().length > 6 };
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
   }
 
   return { value: "", isValid: false };
@@ -49,7 +49,7 @@ const Login = (props) => {
     isValid: null,
   });
 
-  const authCtx= useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
 
   //useEffect summary
   /* useEffect will run one time  and  bases on  dependencies cleanup function will run  (it will clear the useeffect)
@@ -61,26 +61,22 @@ const Login = (props) => {
       console.log("Clean Up function ");
     };
   }, [passwordState.value]);
-  
-const { isValid : emailIsValid } = emailState;
-const { isValid : passwordIsValid  } = passwordState;
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
 
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("checking form validity!");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
 
-    useEffect(() => {
-      const identifier = setTimeout(() => {
-        console.log("checking form validity!");
-        setFormIsValid(
-          emailIsValid    && passwordIsValid
-        );
-      }, 500);
-
-      //clean up function
-      return () => {
-        console.log('Cleanup');
-        clearTimeout(identifier);
-      };
-    }, [emailIsValid, passwordIsValid]);
+    //clean up function
+    return () => {
+      console.log("Cleanup");
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     // setEnteredEmail(event.target.value);
@@ -89,13 +85,13 @@ const { isValid : passwordIsValid  } = passwordState;
     // setFormIsValid(
     //   //event.target.value.includes("@") && enteredPassword.trim().length > 6
     //  // event.target.value.includes("@") && passwordState.value.trim().length > 6 //pwd
-    //   event.target.value.includes("@") && passwordState.isValid 
+    //   event.target.value.includes("@") && passwordState.isValid
     // );
   };
 
   const passwordChangeHandler = (event) => {
     //setEnteredPassword(event.target.value);
-      dispatchPassword({type : "USER_INPUT", val : event.target.value}); //pwd
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value }); //pwd
 
     setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
@@ -106,53 +102,38 @@ const { isValid : passwordIsValid  } = passwordState;
   };
 
   const validatePasswordHandler = () => {
-   // setPasswordIsValid(enteredPassword.trim().length > 6);
-   // setPasswordIsValid(passwordState.value.trim().length > 6);  //pwd
-   dispatchPassword({type : "INPUT_BLUR"}); //PWD
-
+    // setPasswordIsValid(enteredPassword.trim().length > 6);
+    // setPasswordIsValid(passwordState.value.trim().length > 6);  //pwd
+    dispatchPassword({ type: "INPUT_BLUR" }); //PWD
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-   // props.onLogin(emailState.value, enteredPassword);
+    // props.onLogin(emailState.value, enteredPassword);
     authCtx.onLogin(emailState.value, passwordState.value); //pwd
-
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            //passwordIsValid === false ? classes.invalid : ""
-            passwordState.isValid === false ? classes.invalid : "" //pwd
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            // value={enteredPassword}
-            value={passwordState.value} // pwd
-
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+            <Input
+          id="password"
+          label="Password"
+          type="empasswordail"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./Store/auth-context";
+ 
 
 ReactDOM.render(
   <AuthContextProvider>
